cf4be78aacd92de876a8371e8d02bee5adc2b771
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-02-24 18:06:50 +0100

    Base project

--- Sources/DiceSPM/DiceSPM.swift
+++ Sources/DiceSPM/DiceSPM.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct DiceSPM {
+    public static func roll() -> String {
+        let values = ["Ace", "J", "K", "Q", "Red", "Black"]
+        return values[Int.random(in: 0...(values.count - 1))]
+    }
+}
--- Tests/DiceSPMTests/DiceSPMTests.swift
+++ Tests/DiceSPMTests/DiceSPMTests.swift
@@ -2,5 +2,13 @@ import Testing
 @testable import DiceSPM
 
 @Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    var dicValues: [String: Int] = ["Ace": 0, "J": 0, "K": 0, "Q": 0, "Red": 0, "Black": 0]
+    for _ in 0..<100 {
+        let result: String = DiceSPM.roll()
+        dicValues[result]! += 1
+    }
+    
+    for value in dicValues.keys {
+        #expect(dicValues[value] ?? 0 > 0)
+    }
 }
